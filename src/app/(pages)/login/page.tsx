764e54114c5e35f1fbaeda0511a1d509a4954a26
Mikael <mikael@Mac-mini-de-Mikael.local>
2025-07-22 19:37:34 -0300

    ajuste no login

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -14,31 +14,7 @@ export default function Login() {
   return (
     <div className="min-h-screen flex" style={{ background: "#f4f3f5" }}>
       <div className="absolute w-full top-0 z-50"><NavbarLogoCenter /></div>
-
-      <div
-        className="hidden lg:block lg:w-1/2 relative overflow-hidden group"
-      >
-        <div
-          className="absolute inset-0 transition-transform duration-1000 ease-in-out will-change-transform group-hover:scale-110 z-0"
-          style={{
-            backgroundImage: 'url(https://img.freepik.com/fotos-gratis/retrato-de-uma-mulher-jogando-poquer-em-um-cassino_23-2151830999.jpg?t=st=1753156805~exp=1753160405~hmac=f14666b73068cb3c68a1c372509e2886bfcbd73ebb0faa0da35024fe35caa8e7&w=2000)',
-            backgroundSize: 'cover',
-            backgroundPosition: 'top',
-          }}
-        />
-        {/* Overlay e texto, sem zoom */}
-        <div className="absolute inset-0 bg-purple-950/30 flex items-center justify-center p-12 z-10 pointer-events-none">
-          <div className="text-white text-center select-none">
-            <h2 className="text-4xl font-bold mb-4">Sobra Mais</h2>
-            <p className="text-xl">
-              Controle suas finanças de forma simples, segura e eficiente. Gerencie receitas, despesas e tenha uma visão completa do seu dinheiro em um só lugar.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      {/* Área de login */}
-      <div className="w-full lg:w-1/2 flex items-center justify-center bg-gray-50 p-4 sm:p-6 lg:p-8">
+      <div className="w-full flex items-center justify-center bg-gray-50 p-4 sm:p-6 lg:p-8">
         <div className="w-full max-w-[600px] mx-auto bg-white p-6 sm:p-8 rounded-lg shadow-md">
           <div className="text-center lg:hidden">
             <h1 className="text-2xl sm:text-3xl font-bold" style={{ color: "#3D195B" }}>
